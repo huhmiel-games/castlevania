0c4da66c7703d96bee540e7a96ffd63170ae32c3
philippe Pereira <phil.pereira@protonmail.com>
2022-12-08 06:07:58 +0100

    added enemy atlas

--- src/custom/preloadAssets.ts
+++ src/custom/preloadAssets.ts
@@ -1,7 +1,8 @@
 export default function preloadAssets(scene: Phaser.Scene)
 {
     scene.load.atlas('atlas', '/assets/graphics/atlas/richter.png', '/assets/graphics/atlas/richter.json');
-    scene.load.atlas('items', '/assets/graphics/atlas/items.png', '/assets/graphics/atlas/items.json')
+    scene.load.atlas('items', '/assets/graphics/atlas/items.png', '/assets/graphics/atlas/items.json');
+    scene.load.atlas('enemies', '/assets/graphics/atlas/enemies.png', '/assets/graphics/atlas/enemies.json'); 
 
     scene.load.spritesheet('health', '/assets/graphics/hud/health-3x8.png', { frameWidth: 3, frameHeight: 7 });
     scene.load.image('weaponFrame', '/assets/graphics/hud/weapon-frame.png');
