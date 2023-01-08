c7b9caba34224772c4c30010cb388f216a3482dc
philippe Pereira <phil.pereira@protonmail.com>
2023-01-08 20:20:31 +0100

    refactor : use acceleration now

--- src/gameobjects/MovingPlatform.ts
+++ src/gameobjects/MovingPlatform.ts
@@ -6,6 +6,7 @@ export default class MovingPlatform extends Phaser.GameObjects.Image
 {
     public scene: GameScene;
     public body: Phaser.Physics.Arcade.Body;
+    private currentAcceleration: number = MOVING_PLATFORM_SPEED;
     constructor(config: { scene: GameScene; x: number; y: number; texture: string | Phaser.Textures.Texture; frame: string | number; })
     {
         super(config.scene, config.x, config.y, config.texture, config.frame);
@@ -19,19 +20,60 @@ export default class MovingPlatform extends Phaser.GameObjects.Image
         this.body.setAllowGravity(false)
             .setImmovable(true)
             .setFriction(1, 0)
-            .setVelocityX(MOVING_PLATFORM_SPEED);
+            .setMaxVelocityX(MOVING_PLATFORM_SPEED);
+
+            const rightTile = this.scene.colliderLayer.getTileAtWorldXY(this.body.right, this.body.center.y);
+
+            if(rightTile && rightTile.canCollide)
+            {
+                this.currentAcceleration = -MOVING_PLATFORM_SPEED;
+            }
     }
 
     public preUpdate(time: number, delta: number)
     {
-        if (this.body.blocked.left)
+        const { body } = this;
+
+        const { acceleration, blocked, bottom } = body;
+
+        if (blocked.left)
         {
-            this.body.setVelocityX(MOVING_PLATFORM_SPEED);
+            this.currentAcceleration = MOVING_PLATFORM_SPEED;
+
+            body.setDragX(0);
         }
 
-        if (this.body.blocked.right)
+        if (blocked.right)
         {
-            this.body.setVelocityX(-MOVING_PLATFORM_SPEED);
+            this.currentAcceleration = -MOVING_PLATFORM_SPEED;
+
+            body.setDragX(0);
         }
+
+        if(acceleration.x > 0 && blocked.none)
+        {
+            const rightTile = this.scene.colliderLayer.getTileAtWorldXY(body.right + 12, bottom);
+
+            if(rightTile?.collideLeft)
+            {
+                this.currentAcceleration = 0;
+
+                body.setDragX(MOVING_PLATFORM_SPEED);
+            }
+        }
+
+        if(acceleration.x < 0 && blocked.none)
+        {
+            const leftTile = this.scene.colliderLayer.getTileAtWorldXY(body.left - 12, bottom);
+
+            if(leftTile?.collideRight)
+            {
+                this.currentAcceleration = 0;
+
+                body.setDragX(MOVING_PLATFORM_SPEED);
+            }
+        }
+
+        body.setAccelerationX(this.currentAcceleration);
     }
 }
