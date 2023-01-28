9ae768aaa68d3c79e3a7397de79cb47a6e5fbada
philippe Pereira <phil.pereira@protonmail.com>
2023-01-28 09:49:16 +0100

    refactor: phaser config in its own file

---
+++ src/phaser-config.ts
@@ -0,0 +1,55 @@
+import { WIDTH, HEIGHT, isDev } from "./constant/config";
+import BootScene from "./scenes/BootScene";
+import GameOverScene from "./scenes/GameOverScene";
+import GameScene from "./scenes/GameScene";
+import HudScene from "./scenes/HudScene";
+import LoadScene from "./scenes/LoadScene";
+import MenuScene from "./scenes/MenuScene";
+import OptionsScene from "./scenes/OptionsScene";
+
+export const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    width: WIDTH,
+    height: HEIGHT,
+    pixelArt: true,
+    autoRound: true,
+    expandParent: true,
+    input: {
+        gamepad: true,
+        mouse: false,
+        windowEvents: false,
+    },
+    plugins: {
+        global: [
+            {
+                //key: 'SceneWatcher', plugin: PhaserSceneWatcherPlugin, start: true
+            },
+            {
+                // key: 'rexVirtualJoystickPlugin',
+                // plugin: VirtualJoystickPlugin,
+                // start: true
+            }
+            // {
+            //     key: 'rexNinePatchPlugin',
+            //     plugin: rexNinePatchPlugin,
+            //     start: true
+            // }
+        ]
+    },
+    scale: {
+        mode: Phaser.Scale.ScaleModes.FIT,
+        autoRound: true,
+        autoCenter: Phaser.Scale.Center.CENTER_HORIZONTALLY,
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            tileBias: 20,
+            gravity: { x: 0, y: 1000 },
+            debug: isDev,
+            debugShowBody: true,
+            debugShowStaticBody: true,
+        },
+    },
+    scene: [BootScene, LoadScene, MenuScene, OptionsScene, GameScene, HudScene, GameOverScene],
+};
